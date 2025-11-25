be62d99d6bec65a48ccc19218637d3c9a507d89c
widedoueslati82 <167637733+widedoueslati82@users.noreply.github.com>
2025-11-25 10:23:52 -0500

    1 er commit pour copier le contenu dans le main du git
    notre premier commit pour mettre le contenu qui se trouve en local dans le main

---
+++ html/search/files_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['hello_2eassemblyinfo_2ecs_0',['hello.AssemblyInfo.cs',['../hello_8_assembly_info_8cs.html',1,'']]],
+  ['hello_2eglobalusings_2eg_2ecs_1',['hello.GlobalUsings.g.cs',['../hello_8_global_usings_8g_8cs.html',1,'']]]
+];
